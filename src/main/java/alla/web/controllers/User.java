808cb156c9faab06e1e320902a5e7d6949345119
Allan Collado <allan30nic@outlook.com>
2022-01-29 20:45:28 -0500

    first upload

---
+++ src/main/java/alla/web/controllers/User.java
@@ -0,0 +1,18 @@
+package alla.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class User {
+
+    @GetMapping({"/", "/home"})
+    public String home(){
+        return "index";
+    }
+   @GetMapping("/resume")
+    public String resume(){
+        return "form";
+    }
+}
